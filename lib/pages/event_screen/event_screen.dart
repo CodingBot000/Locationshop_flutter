0cb58a8a0633afeca89a99411a552ee6158193aa
CodingBot000 <pokerface582@gmail.com>
2024-09-30 02:28:44 +0900

    remove useless code

--- lib/pages/event_screen/event_screen.dart
+++ lib/pages/event_screen/event_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:location_shop/model/event_data.dart';
-import 'package:location_shop/repository/respository.dart';
 
 import '../../component/top_app_bar_sub.dart';
 import 'event_list_widget.dart';
@@ -12,8 +10,7 @@ class EventScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<EventData> list = DataRepository.instance.getEventAllDatas();
-    print('qqq q q q q q q q q ');
+
     return const Scaffold(
         backgroundColor: Colors.white,
         appBar: TopAppBarSub(title: "EVENT"),
